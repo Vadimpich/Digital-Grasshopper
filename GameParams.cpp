b6a520fc9148a06065ef23e505f7ffebe6a4dad6
asnikov555 <126055951+asnikov@users.noreply.github.com>
2023-04-04 13:45:55 +0300

    Update GameParams.cpp

--- GameParams.cpp
+++ GameParams.cpp
@@ -1,1 +1,50 @@
+#include <iostream>
 #include "GameParams.h"
+using namespace std;
+
+class GameParams{
+
+private:
+    int rowCount;
+    int colCount;
+    int diff;
+
+public:
+    GameParams(int rowCount, int colCount, int diff) {                                                         ?
+        this->rowCount = rowCount;
+        this->colCount = colCount;
+        this->diff = diff;
+    }
+
+    GameParams() {
+        this->rowCount = 6;
+        this->colCount = 6;
+        this->diff = 1;
+    }
+
+    int getColCount() {
+        return colCount;
+    }
+
+    void setColCount(int colCount) {
+        this->colCount = colCount;
+    }
+
+    int getRowCount() {
+        return rowCount;
+    }
+
+    void setRowCount(int rowCount) {
+        this->rowCount = rowCount;
+    }
+
+    int getDiff() {
+        return diff;
+    }
+
+    void setDiff(int diff) {
+        this->diff = diff;
+    }
+
+
+};
